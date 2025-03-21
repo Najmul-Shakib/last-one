26c97ab6ed89ac15bf1301c2e65c721a543ff011
saki <najmulshakibf@gmail.com>
2025-03-21 16:24:32 +1100

    let's see

--- clearHistory.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('clear').addEventListener('click', function(){
-
-    document.getElementById('container').innerText="";
-})
--- devBoard.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('dev-board-box').addEventListener('click', function(){
-
-    window.location.href="lasttask.html"
-})
