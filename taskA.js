619010b9684c2529de3b522ec41b4dc9e58a6f0f
saki <najmulshakibf@gmail.com>
2025-03-21 16:30:23 +1100

    l

---
+++ taskA.js
@@ -0,0 +1,16 @@
+
+document.getElementById('btn-completed-1').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById('task-number').innerText;
+    // console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal - 1;
+    console.log(valInput);
+    document.getElementById('task-number').innerHTML = `<p>0${valInput}</p>`;
+
+    
+    let btn = document.getElementById("btn-completed-1");
+    btn.disabled = true;
+    btn.innerText = "Completed";
+})
